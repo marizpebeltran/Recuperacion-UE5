ae34ae1a356ec3780b8193dd74406b7a1c059d50
marizpebeltran <marizpebeltran@gmail.com>
2026-04-19 14:46:20 -0600

    Arquitectura: Inicializacion del proyecto y configuracion de gitignore

---
+++ Source/recuperacion/recuperacion.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "recuperacion.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, recuperacion, "recuperacion" );
